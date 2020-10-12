d60d1f4e5a9159dc4fa56ed9dc3d11c1072b560f
yvan-burrie <yvan.burrie@hotmail.com>
2020-10-12 11:39:31 +1000

    Add Passport Vue components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,13 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <!-- let people make clients -->
+                    <passport-clients></passport-clients>
+                    <!-- list of clients people have authorized to access our account -->
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <!-- make it simple to generate a token right in the UI to play with -->
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
             </div>
         </div>
