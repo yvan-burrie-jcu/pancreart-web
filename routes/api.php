af1c37883c54dc942a5c07ecbb451a92cf16cfae
yvan-burrie <yvan.burrie@hotmail.com>
2020-10-30 22:18:57 +1000

    Fix controller calls for events routes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request)
+{
     return $request->user();
 });
 
-Route::post('events/get', [EventController::class, 'getEvents']);
-Route::post('events/add', [EventController::class, 'addEvents']);
+Route::post('events/get', 'ApiController@getEvents');
+Route::post('events/add', 'ApiController@addEvents');
